979436ce50e1e0e7df16e6621b885b7cdf2299ec
CodyNeo <abdallahomm123@gmail.com>
2026-08-05 15:05:18 +0200

    FP-9 configure code quialty tools

--- backend/alembic/versions/9b2e22030c10_initial_migration.py
+++ backend/alembic/versions/9b2e22030c10_initial_migration.py
@@ -1,18 +1,15 @@
 """Initial migration
 
 Revision ID: 9b2e22030c10
-Revises: 
+Revises:
 Create Date: 2026-08-05 13:29:46.869252
 
 """
+
 from typing import Sequence, Union
 
-from alembic import op
-import sqlalchemy as sa
-
-
 # revision identifiers, used by Alembic.
-revision: str = '9b2e22030c10'
+revision: str = "9b2e22030c10"
 down_revision: Union[str, Sequence[str], None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
--- backend/app/api/health.py
+++ backend/app/api/health.py
@@ -5,6 +5,4 @@ router = APIRouter(tags=["Health"])
 
 @router.get("/health")
 async def health_check():
-    return {
-        "status": "healthy"
-    }
+    return {"status": "healthy"}
